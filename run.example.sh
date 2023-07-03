578031a54819343fd2d529800586186deed29967
feryadi-amartha <feryadi@amartha.com>
2023-07-03 08:39:02 +0700

    add example of run.sh

---
+++ run.example.sh
@@ -0,0 +1,14 @@
+# set config to env
+export NAME=go-url-shortener
+export PORT=8080
+
+export DB_NAME=
+export DB_HOST=
+export DB_PORT=
+export DB_USER=
+export DB_PASS=
+
+export NEW_RELIC_LICENSE_KEY=
+
+# run go application
+go run cmd/api/main.go
